499c48a53e0968842d3a4f14f7fbdfc771299e88
linux-kbl <edgardo.torres@hotmail.com>
2021-01-22 06:10:20 -0600

    generated a numRandom function

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -13,15 +13,28 @@ const Formulario = () => {
   const handleChange = (e) => {
     // Cuando los inputs cambian se ejecuta esta funcion
     //...cita, crea una copia, esto porque cuando ecribias en otro input se remplazaba por lo ultimo que escribia
-    // [e.target.name]: con esto le decimos que el name va a tenenr el valor e.target.value
+    // [e.target.name]: con esto tomamos el valor que tenga name en los inputs, le decimos que el name va a tenenr el valor e.target.value
     setCita({
       ...cita,
       [e.target.name]: e.target.value
     })
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
+
+  const numRandom = () => {
+    //Make a random number
+    return Math.floor(Math.random() * (10000 - 0)) + 0;
+  }
+
   return(
     
-    <form>
+    <form
+      onSubmit={handleSubmit}
+    >
       <label htmlFor="mascota">Nombre Mascota</label>
       <input 
         id="mascota"
@@ -34,23 +47,27 @@ const Formulario = () => {
         id="dueño"
         type="text"
         name="dueño"
+        onChange={handleChange}
       />
       <label htmlFor="fecha">Fecha</label>
       <input 
         id="fecha"
         type="date"
         name="fecha"
+        onChange={handleChange}
       />
       <label htmlFor="hora">Hora</label>
       <input 
         id="hora"
         type="date"
         name="hora"
+        onChange={handleChange}
       />
       <label htmlFor="sintomas">Síntomas</label>
       <textarea
         id="sintomas"
         name="sintomas"
+        onChange={handleChange}
       ></textarea>
       <button 
         type="submit"
