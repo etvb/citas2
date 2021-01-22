63303e800a0f9166531b17377cad51871414e36c
linux-kbl <edgardo.torres@hotmail.com>
2021-01-21 20:35:18 -0600

    create form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,45 @@ import React from "react";
 
 const Formulario = () => {
   return(
-    <h2>desde Formulario</h2>
+    
+    <form>
+      <label for="mascota">Nombre Mascota</label>
+      <input 
+        id="mascota"
+        type="text"
+        name="mascota"
+      />
+      <label for="dueño">Nombre Dueño</label>
+      <input 
+        id="dueño"
+        type="text"
+        name="dueño"
+      />
+      <label for="fecha">Fecha</label>
+      <input 
+        id="fecha"
+        type="date"
+        name="fecha"
+      />
+      <label for="hora">Hora</label>
+      <input 
+        id="hora"
+        type="date"
+        name="hora"
+      />
+      <label for="sintomas">Síntomas</label>
+      <input 
+        id="sintomas"
+        type="date"
+        name="sintomas"
+      />
+      <button 
+        type="submit"
+      >
+        Agregar Cita
+      </button>
+
+    </form>
 
   );
 }
