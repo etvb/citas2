9036b60b4b594ba4fb9e3d32d5d8968577cbbb2e
linux-kbl <edgardo.torres@hotmail.com>
2021-01-22 21:48:48 -0600

    use localStorage with useEfect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Formulario from './components/Formulario';
 import ShowCitas from './components/ShowCitas';
 
 
 function App() {
-  const [citas, setCitas] =useState([]);
+
+  let citasLocalStorage = JSON.parse(localStorage.getItem('citas'));
+  // if(!citasLocalStorage){
+  //   console.log('No tengo localstorage');
+  //   //si no tengo citas en localStorage entonces voy a guardar un arreglo vacio 
+  //   citasLocalStorage = [];
+  // }
+
+  const [citas, setCitas] =useState(citasLocalStorage);
 
   const addCitas = (infoCita) => {
-    console.log(infoCita);
     setCitas([...citas,infoCita]);
   }
-
+  
   const delCita = (id) => {
-    console.log(id)
-
     let newCitas = citas.filter((cita) => {
       return  cita.id !== id
     })
     setCitas(newCitas);
   }
+  
+  //LOCALSTORAGE: Cada que cambien el state citas tenemos que hacer que el localStorage se actualiza para esto 
+  //usamos useEffect, este se ejecuta cuando esta lista la pag, y observa los cambios en un elemento especifico
+  //en este caso el elemento especifico seria citas
+
+  useEffect( () => {
+    if(citasLocalStorage){
+      //si tengo citas en localStorage voy a pasarle como string el state citas
+      localStorage.setItem('citas', JSON.stringify(citas))
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]))
+    }    
+
+  },[citas]);
 
   return (
     <>
